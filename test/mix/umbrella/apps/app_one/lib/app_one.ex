d1c71989ee971c6d194212adb7e921f0dc034201
russell <russell@brex.com>
2019-06-22 23:56:45 +0000

    test new deps system

--- test/mix/umbrella/apps/app_one/lib/app_one.ex
+++ test/mix/umbrella/apps/app_one/lib/app_one.ex
@@ -5,13 +5,16 @@ defmodule AppOne do
 
   @nothing IO.inspect(Application.get_all_env(:app_one), label: "app one compile-time config")
 
+  @compile_time_json Jason.encode(%{ok: "cool"})
+
   def hello do
     answer = Common.add(99, 14)
     IO.puts("app one; answer = #{answer}")
-    #v = Common.Macros.my_macro("input")
+    IO.inspect(@compile_time_json, label: "compile time json encode")
     v = AppOne.Macros.call_common_function(1, 3)
     IO.puts("v = #{inspect(v)}")
-    enc= Jason.encode(Enum.to_list(1..10))
+    enc = Jason.encode(Enum.to_list(1..10))
+    IO.inspect(CommonLibrary.hello, label: "common library says")
     enc
   end
 end
--- test/mix/umbrella/apps/app_one/mix.exs
+++ test/mix/umbrella/apps/app_one/mix.exs
@@ -9,7 +9,7 @@ defmodule AppOne.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.9-dev",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
